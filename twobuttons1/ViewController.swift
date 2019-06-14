5d452c6eea3c762296ea44663012dfb453627fe8
Genesis <genesis@csit-11.local>
2019-06-14 13:42:44 -0400

    Initial Commit

---
+++ twobuttons1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twobuttons1
+//
+//  Created by Genesis on 6/14/19.
+//  Copyright © 2019 Genesis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
